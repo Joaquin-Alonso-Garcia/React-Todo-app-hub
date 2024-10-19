f44a59e05989a230fc471a9c38d56b508d7bd197
Joaquin <joaco.alonso.garcia@gmail.com>
2024-10-19 13:34:58 -0500

    checkpoint tailwind installed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1 className='text-3xl font-bold'>TODO</h1>
+        <img src="" alt="" />
+      </header>
+    </>
+  )
+}
+
+export default App;
